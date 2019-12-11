c80c08b3cf868d70914def15f89531eaacd61707
Daniel Ocampo <ochampo77@gmail.com>
2019-12-10 21:28:44 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LINQExamplesQueries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = { "Tom", "Dick", "Harry" }; 
+            IEnumerable<string> filteredNames = System.Linq.Enumerable.Where(names, n => n.Length >= 4); 
+            foreach (string n in filteredNames) 
+                Console.WriteLine(n);
+
+        }
+    }
+}
